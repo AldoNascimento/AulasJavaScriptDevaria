909bffaa6d5cecc3833109a43af449f6c16cd3ed
Aldo Nascimento <aldonpaula@gmail.com>
2021-04-27 21:29:13 -0300

    Estruturas de Seleção

---
+++ estrutura-selecao.js
@@ -0,0 +1,11 @@
+const valido = true;
+let nome ="Aldo"
+let idade = 20
+
+if(valido) {
+    console.log("Expressão valida Nome = " + nome, " e idade = ", + idade);
+}
+if(valido) {
+    console.log(" Segundo if idade = " + idade);
+
+}
