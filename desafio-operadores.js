b9691b439dd3047494200906025d283012d03c16
Aldo Nascimento <aldonpaula@gmail.com>
2021-04-28 12:22:35 -0300

    Calculadora Js

---
+++ desafio-operadores.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+
+const leitor = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout
+});
+
+const primeiraPergunta = "Digite o primeiro número:\n";
+const segundaPergunta = "Digite o segundo número:\n";
+const terceiraPergunta = "Digite a operação:\n";
+
+const realizarCauculo = (num1, num2, operacao) => {
+    const primeiroNumero = parseInt (num1);
+    const segundoNumero = parseInt (num2);
+    switch (operacao) {
+        case "+":
+            return primeiroNumero + segundoNumero;
+        case "-":
+            return primeiroNumero - segundoNumero; 
+        case "*":
+            return primeiroNumero + segundoNumero;
+        case "/":
+            return primeiroNumero / segundoNumero;     
+        default:
+            return 0;             
+    }
+}
+
+leitor.question(primeiraPergunta, (primeiroNumero) => {
+    leitor.question(segundaPergunta, (segundoNumero) => {
+        leitor.question(terceiraPergunta, (operacao) => {
+            const resultado =realizarCauculo(primeiroNumero, segundoNumero, operacao);
+            console.log({resultado});
+            leitor.close();
+        });
+
+});
+
+});
